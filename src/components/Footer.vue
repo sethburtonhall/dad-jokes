aeeeedb4d89269752d51f81e5ab9bb308c3a3844
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2021-06-20 20:05:45 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-full p-12 space-y-2 text-gray-900 lg:justify-between lg:space-y-0 lg:flex-row dark:text-white dark:bg-gray-900 ">
+  <div class="flex flex-col items-center w-full p-12 space-y-2 text-base text-gray-900 lg:justify-between lg:space-y-0 lg:flex-row dark:text-white dark:bg-gray-900 ">
     <div class="flex flex-col space-y-2 text-center lg:items-start">
       <div>
         Made with 💖 by <a href="https://twitter.com/sethburtonhall/status/1403176619972386816" target="_blank" rel="noopener">Seth Hall</a> as a part of <a href="https://virtualcoffee.io" target="_blank" rel="noopener" class="text-pink-500">VirtualCoffee's</a> <a href="https://virtualcoffee.io/monthlychallenges/june-2021/" target="_blank" rel="noopener">Monthly Challenge</a>
@@ -15,7 +15,7 @@ export default {
       </div>
     </div>
 
-    <div class="flex items-center justify-center space-x-2 lg:justify-start lg:items-start">
+    <div class="flex flex-col items-center justify-center space-x-2 lg:flex-row lg:justify-start lg:items-start">
       <a href="https://twitter.com/search?q=%23buildinginpublic&src=typeahead_click" target="_blank" rel="noopener">
         <pre class="inline">#buildinginpublic</pre>
       </a>
